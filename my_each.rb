708dbc4399b360a540e4a1e6d654672aec7ffc2f
Utshant Gurung <utshantgurung@gmail.com>
2021-05-28 07:06:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  i = 0
+  while i < array.size
+    yield array[i]
+    i += 1
+  end
 end
+
+my_each { puts array}
